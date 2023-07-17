dce0b414d413efe281b0152617fabc646b7a6462
BorgTb <bestowifer@gmail.com>
2023-07-17 18:50:30 -0400

    agregar funcionalidad de covid escondido en el tablero y al clickearlo perder y resetear

--- js/script.js
+++ js/script.js
@@ -5,14 +5,52 @@ function createGrid() {
         for (let j = 0; j < 9; j++) {
             const cell = document.createElement('div');
             cell.classList.add('cell');
-            cell.innerText = `${i + 1}, ${j + 1}`;
+            cell.innerText = ' ';
             grid.appendChild(cell);
         }
     }
 }
 
+function spawnVirus() {
+    const cell = document.getElementsByClassName('cell');
+    
+
+    for(let i = 0 ; i < 10; i++) {
+        //make random number between 0-15
+        let pos = Math.floor(Math.random() * 81);
+        cell.item(pos).classList.add('bomb');
+        cell.item(pos).innerText = ' ';
+    }
+}
+
+
+function click(cell) {
+    if (cell.classList.contains('bomb')) {
+        cell.classList.add('bomb');
+        cell.innerText = '🦠';
+        setTimeout(() => {
+            alert('Game Over');
+            location.reload();
+        }, 1000);   
+    }else{
+
+    }
+}
+
+function revelarCasillas(){
+    
+}
+
+
+
+grid.addEventListener('click', (event) => {
+    const cell = event.target;
+    click(cell);
+    revelarCasillas(cell);
+});
 
 
 
 createGrid();
-console.log(document.getElementsByClassName('cell'));
+spawnVirus();
+
